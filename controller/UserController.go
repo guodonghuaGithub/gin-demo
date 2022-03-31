5e1b5de03150a8192c7ca146f91c8def01e59581
guodonghua <1920350018.com>
2022-03-31 22:49:46 +0800

    打开session

--- controller/UserController.go
+++ controller/UserController.go
@@ -17,13 +17,13 @@ func (user *UserController) Login(c *gin.Context) {
 	var UserParam model.GetParams //定义参数绑定
 	err := c.BindJSON(&UserParam) //接收机参数
 	if err != nil {
-		tool.Failed(c, 0 ,"参数解析失败")
+		tool.Failed(c, 0, "参数解析失败")
 		return
 	}
 	ms := service.UserService{}
 	result := ms.Login(UserParam)
 	if result == nil {
-		tool.Failed(c,0, result)
+		tool.Failed(c, 0, result)
 		return
 	}
 	jwt := jwt.JWT{}
@@ -32,6 +32,8 @@ func (user *UserController) Login(c *gin.Context) {
 		tool.Failed(c, -1, "token生成失败")
 		return
 	}
+	//value, _ := json.Marshal(result)
+	//tool.SetSession(c, "user_"+string(result.Id), value)
 	tool.Success(c, result)
 }
 
@@ -40,15 +42,14 @@ func (user *UserController) Modify(ctx *gin.Context) {
 	err := ctx.ShouldBindBodyWith(&UserParams, binding.JSON)
 	if err != nil {
 		fmt.Println(err.Error())
-		tool.Failed(ctx,0, "参数解析失败")
+		tool.Failed(ctx, 0, "参数解析失败")
 		return
 	}
 	service := service.UserService{}
 	result := service.Modify(UserParams)
 	if result == false {
-		tool.Failed(ctx,0, "更新失败")
+		tool.Failed(ctx, 0, "更新失败")
 		return
 	}
 	tool.Success(ctx, "更新成功")
 }
-
